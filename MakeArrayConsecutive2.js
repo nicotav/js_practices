b037d5f282b0858d83b53a63c9d30a0ef57461eb
nicotav <nicolastaveras01@gmail.com>
2023-01-26 21:50:52 -0400

    modified:   MakeArrayConsecutive2.js -> changing the algorithm to be more efficient

--- MakeArrayConsecutive2.js
+++ MakeArrayConsecutive2.js
@@ -1,20 +1,15 @@
-function solution(statues) {
-    let result = 0
-    let sorted = statues.sort()
-    let first = sorted[0]
-    let last = sorted[sorted.length - 1]
-
-    for (let i = first; i < last; i++) {
-        if (!sorted.includes(i)) {
-            result++
+function makeArrayConsecutive2(statues) {
+    let statuesNeeded = 0;
+    // Sort array smallest to largest
+    statues.sort((a, b) => {
+        return a - b;
+    })
+    // Iterate through array and find gaps in values
+    for (let i = 0; i < statues.length; i++) {
+        // If there is a gap between neighboring numbers subtract higher number from lower number i.e. [3, 6] is 6 - 3. There is a gap of 3, so 2 numbers are missing i.e. 4, 5
+        if (statues[i + 1] - statues[i] > 1) {
+            statuesNeeded += statues[i + 1] - statues[i] - 1;
         }
     }
-
-    return result
-
+    return statuesNeeded;
 }
-
-
-
-solution([6, 2, 3, 8]) // [2, 3, 6, 8]
-console.log('hi')
