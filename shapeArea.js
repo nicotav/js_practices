5facef3bfc5e6d6298865cd6efa5188a98e39a77
nicotav <nicolastaveras01@gmail.com>
2023-01-26 20:57:10 -0400

    new file:   shapeArea.js - > calculate the area of a n-polygon

---
+++ shapeArea.js
@@ -0,0 +1,23 @@
+function solution(n) {
+    let result = (n * n) + (n - 1) * (n - 1)
+
+    return result
+}
+
+
+
+solution(8999) // 161946005
+solution(8998) // 161944005
+solution(8997) // 161942005
+solution(1) // 1
+solution(2) // 5
+solution(3) // 13
+solution(4) // 25
+solution(5) // 41
+solution(6) // 61
+solution(7) // 85
+solution(8) // 113
+solution(9) // 145
+solution(10) // 181
+solution(11) // 221
+
